03090eecccb2deb938102609c80e1649cc38dac7
asfafafaf1 <js1puppyjul@hotmail.com>
2024-05-12 20:04:44 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const insertionSortRecursive = (arr, n = arr.length) => {
-  if (n <= 1) {
-    return arr;
-  }
-  insertionSortRecursive(arr, n - 1);
-  const last = arr[n - 1];
-  let j = n - 2;
-  while (j >= 0 && arr[j] > last) {
-    arr[j + 1] = arr[j];
-    j--;
-  }
-  arr[j + 1] = last;
-  return arr;
-};
+function sayHi() {
+  console.log("Hi!");
+}
+
+setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
