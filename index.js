63772378e2252d83962d8cea3540211e0e3fdb9d
asfafafaf1 <js1puppyjul@hotmail.com>
2024-05-13 21:57:59 +0700

    demo

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function sayHi() {
-  console.log("Hi!");
+function combinationSum(candidates, target) {
+  const result = [];
+  backtrack([], 0, 0);
+  return result;
+  function backtrack(combination, start, sum) {
+    if (sum === target) {
+      result.push([...combination]);
+      return;
+    }
+    if (sum > target) return;
+    for (let i = start; i < candidates.length; i++) {
+      combination.push(candidates[i]);
+      backtrack(combination, i, sum + candidates[i]);
+      combination.pop();
+    }
+  }
 }
-
-setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
